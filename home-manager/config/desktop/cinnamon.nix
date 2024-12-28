481f953cc941d3d9e852f30a3cb224acc0ec67d9
benny@MGC-GIT-01 <ben.r.harris@protonmail.com>
2024-12-28 16:17:29 +0930

    testing

--- home-manager/config/desktop/cinnamon.nix
+++ home-manager/config/desktop/cinnamon.nix
@@ -38,10 +38,7 @@
     StartWhonix = lib.mkIf osConfig.megacorp.virtualisation.whonix.enable {
       name = "Start Whonix";
       exec = ''
-        sh -c
-        "${pkgs.virtualbox}/bin/VBoxManage startvm Whonix-Gateway-Xfce --type headless &&
-        sleep 1 &&
-        ${pkgs.virtualbox}/bin/VBoxManage startvm Whonix-Workstation-Xfce"
+        "${pkgs.virtualbox}/bin/VBoxManage startvm Whonix-Gateway-Xfce --type headless && sleep 1"
         '';
       terminal = false;
     };
