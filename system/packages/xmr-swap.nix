e07ff535b3efde2121c9369eafc04d5d4772cbff
benny@MGC-GIT-01 <ben.r.harris@protonmail.com>
2024-12-28 15:03:34 +0930

    using pkgs.fetchurl

--- system/packages/xmr-swap.nix
+++ system/packages/xmr-swap.nix
@@ -1,9 +1,9 @@
-{ pkgs, fetchurl, ... }:
+{ pkgs, ... }:
 let
   pname = "UnstoppableSwap";
   version = "1.0.0-rc.11";
 
-  src = fetchurl {
+  src = pkgs.fetchurl {
     url = "https://github.com/UnstoppableSwap/core/releases/download/${version}/${pname}_${version}_amd64.AppImage";
     hash = "";
   };
