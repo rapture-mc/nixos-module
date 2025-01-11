47a939429ff10324a0b038e102943cda128574bd
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-01-11 12:31:29 +0930

    adding keybindings for terminal and browser

--- home-manager/config/desktop/hyprland.nix
+++ home-manager/config/desktop/hyprland.nix
@@ -1,5 +1,16 @@
 _: {
   wayland.windowManager.hyprland = {
     enable = true;
+
+    settings = {
+      "$mainMod" = "SUPER";
+      "$terminal" = "kitty";
+      "$browser" = "firefox";
+
+      bind = [
+        "$mainMod, W, exec $browser"
+        "$mainMod, Q, exec $terminal"
+      ];
+    };
   };
 }
