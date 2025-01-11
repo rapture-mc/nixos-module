ce54d1bd7e8f8c0daa1ac49ec99f98b6afbb6d51
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-01-11 15:15:41 +0930

    adding startup-script

--- home-manager/config/desktop/hyprland.nix
+++ home-manager/config/desktop/hyprland.nix
@@ -1,8 +1,18 @@
-_: {
+{ pkgs, ... }: let
+  startupScript = pkgs.writeShellScriptBin "startupScript" ''
+    ${pkgs.swww}/bin/swww init &
+
+    sleep 1
+
+    ${pkgs.swww}/bin/swww img ${./desktop-wallpaper.jpg} &
+  '';
+in {
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
+      exec-once = "${startupScript}/bin/startupScript";
+
       "$mainMod" = "SUPER";
       "$terminal" = "kitty";
       "$browser" = "firefox";
