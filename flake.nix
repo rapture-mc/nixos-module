c525b351e4e379e493374ad544ed5f754f338cef
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-01-11 10:31:42 +0930

    adding hyprland inputs

--- flake.nix
+++ flake.nix
@@ -15,6 +15,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixvim = {
       url = "github:nix-community/nixvim/nixos-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
