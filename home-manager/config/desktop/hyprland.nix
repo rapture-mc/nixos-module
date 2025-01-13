2c909790912c45d374cf0be448c490aa6141ab24
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-01-13 20:37:38 +0930

    just using someone elses waybar config

--- home-manager/config/desktop/hyprland.nix
+++ home-manager/config/desktop/hyprland.nix
@@ -60,44 +60,90 @@ in {
 
   programs.waybar = {
     enable = true;
-    style = ./waybar.css;
-    settings = {
-      mainBar = {
-        layer = "bottom";
-        position = "bottom";
-        mod = "dock";
-        exclusive = true;
-        gtk-layer-shell = true;
-        margin-bottom = -1;
-        passthrough = false;
-        modules-left = [
-          "custom/os_button"
-          "hyprland/workspaces"
-          "wlr/taskbar"
-        ];
-        modules-center = [];
-        modules-right = [
-          "cpu"
-          "temperature"
-          "memory"
-          "disk"
-          "tray"
-          "pulseaudio"
-          "network"
-          "battery"
-          "hyprland/language"
-          "clock"
-        ];
-        "hyprland/language" = {
-          format = "{}";
-          format-en = "ENG";
-          format-ru = "PYC";
-        };
-        "cpu" = {
-          interval = 5;
-          max-length = 10;
+    systemd.enable = true;
+    style = ''
+      ${builtins.readFile "${pkgs.waybar}/etc/xdg/waybar/style.css"}
+
+      window#waybar {
+        background: transparent;
+        border-bottom: none;
+      }
+
+      * {
+        font-size: 18px;
+      }
+    '';
+    settings = [{
+      height = 30;
+      layer = "top";
+      position = "bottom";
+      tray = { spacing = 10; };
+      modules-center = [ "sway/window" ];
+      modules-left = [ "sway/workspaces" "sway/mode" ];
+      modules-right = [
+        "pulseaudio"
+        "network"
+        "cpu"
+        "memory"
+        "temperature"
+        "battery"
+      ]
+      ++ [
+        "clock"
+        "tray"
+      ];
+      battery = {
+        format = "{capacity}% {icon}";
+        format-alt = "{time} {icon}";
+        format-charging = "{capacity}% ";
+        format-icons = [ "" "" "" "" "" ];
+        format-plugged = "{capacity}% ";
+        states = {
+          critical = 15;
+          warning = 30;
         };
       };
-    };
+      clock = {
+        format-alt = "{:%Y-%m-%d}";
+        tooltip-format = "{:%Y-%m-%d | %H:%M}";
+      };
+      cpu = {
+        format = "{usage}% ";
+        tooltip = false;
+      };
+      memory = { format = "{}% "; };
+      network = {
+        interval = 1;
+        format-alt = "{ifname}: {ipaddr}/{cidr}";
+        format-disconnected = "Disconnected ⚠";
+        format-ethernet = "{ifname}: {ipaddr}/{cidr}   up: {bandwidthUpBits} down: {bandwidthDownBits}";
+        format-linked = "{ifname} (No IP) ";
+        format-wifi = "{essid} ({signalStrength}%) ";
+      };
+      pulseaudio = {
+        format = "{volume}% {icon} {format_source}";
+        format-bluetooth = "{volume}% {icon} {format_source}";
+        format-bluetooth-muted = " {icon} {format_source}";
+        format-icons = {
+          car = "";
+          default = [ "" "" "" ];
+          handsfree = "";
+          headphones = "";
+          headset = "";
+          phone = "";
+          portable = "";
+        };
+        format-muted = " {format_source}";
+        format-source = "{volume}% ";
+        format-source-muted = "";
+        on-click = "pavucontrol";
+      };
+      "sway/mode" = { format = ''<span style="italic">{}</span>''; };
+      temperature = {
+        critical-threshold = 80;
+        format = "{temperatureC}°C {icon}";
+        format-icons = [ "" "" "" ];
+      };
+    }];
   };
 }
