09f7ad844179040dfc19cf1fcdcef65444820785
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-08 23:03:56 +0930

    switching back to hashed

--- modules/hardening/bootloader.nix
+++ modules/hardening/bootloader.nix
@@ -15,11 +15,16 @@ in {
     password-file = mkOption {
       type = types.str;
       default = "/run/secrets/grub-admin-password";
-      description = "The path to the file containing the grub admin password";
+      description = ''
+        The path to the hashed password file for the grub admin user.
+        Password is hashed because /boot/grub/grub.cfg is readable by anyone.
+
+        Use "grub-mkpasswd-pbkdf2" (available in the grub2 package) to generate the password.
+      '';
     };
   };
 
   config = lib.mkIf cfg.enable {
-    boot.loader.grub.users.${config.megacorp.config.users.admin-user}.passwordFile = cfg.password-file;
+    boot.loader.grub.users.${config.megacorp.config.users.admin-user}.hashedPasswordFile = cfg.password-file;
   };
 }
