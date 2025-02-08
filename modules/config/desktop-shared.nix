5a51aa7f23136a9d425c6b3568af95de58a824f1
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-08 13:40:58 +0930

    updating nerdfont package

--- modules/config/desktop-shared.nix
+++ modules/config/desktop-shared.nix
@@ -2,10 +2,18 @@
   config = {
     networking.networkmanager.enable = true;
 
+    # fonts = {
+    #   enableDefaultPackages = true;
+    #   packages = with pkgs; [
+    #     (nerdfonts.override {fonts = ["Terminus"];})
+    #     noto-fonts-emoji
+    #   ];
+    # };
+
     fonts = {
       enableDefaultPackages = true;
       packages = with pkgs; [
-        (nerdfonts.override {fonts = ["Terminus"];})
+        nerd-fonts.Terminus
         noto-fonts-emoji
       ];
     };
