ffe1b83f686eb63967f945d7b9cf1b790ba76622
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-15 10:36:39 +0930

    adding tmux and nix-list-generations alias

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -13,6 +13,13 @@
       shellAliases = {
         v = "vi";
         ra = "ranger";
+        nix-list-generations = "nix profile history --profile /nix/var/nix/profiles/system";
+
+        # No tmux plugin exists for nushell (as oppose to oh-my-zsh's tmux plugin so need to manually declare them)
+        ta = "tmux attach -t";
+        tkss = "tmux kill-session -t";
+        tl = "tmux list-sessions";
+        ts = "tmux new-session -s";
       };
     };
 
