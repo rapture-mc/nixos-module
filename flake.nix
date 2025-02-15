a987552f074441bdce08775adc9dced7b880edf0
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-15 11:26:58 +0930

    testing nixpkgs version

--- flake.nix
+++ flake.nix
@@ -11,12 +11,12 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-24.11";
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
