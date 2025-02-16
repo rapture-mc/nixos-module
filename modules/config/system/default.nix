e9c9a2af3619e5fdac49cd40e11d52f02e21d172
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-16 15:55:00 +0930

    removing allowUnfree declerationn

--- modules/config/system/default.nix
+++ modules/config/system/default.nix
@@ -51,8 +51,6 @@ in {
 
     home-manager.backupFileExtension = "backup";
 
-    nixpkgs.config.allowUnfree = true;
-
     nix.settings = {
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "@wheel"];
