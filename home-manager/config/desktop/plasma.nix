14f660fb77c97165ee3c6fc1fc4f74ef04cd6f49
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-04-12 13:02:54 +0930

    adding cyberpunk color theme

--- home-manager/config/desktop/plasma.nix
+++ home-manager/config/desktop/plasma.nix
@@ -1,11 +1,36 @@
-_:
-
 {
+  pkgs,
+  ...
+}: let
+cyberpunk-theme = pkgs.stdenv.mkDerivation {
+  name = "theme";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "Roboron3042";
+    repo = "Cyberpunk-Neon";
+    rev = "eb7595459c0d4164262e0ccaf8d6e5c1936a6f67";
+    sha256 = "sha256-whHBIxEGGvTPVUaE/HQDb/Qyl5sPMGlOmofgNCBaNng=";
+  };
+
+  installPhase = ''
+    mkdir $out
+
+    cp -rv $src/kde/cyberpunk-neon.colors $out
+  '';
+};
+in {
+  home.file."cyberpunk-theme" = {
+    enable = true;
+    source = "${cyberpunk-theme}/cyberpunk-neon.colors";
+    target = ".local/share/color-schemes/cyberpunk-neon.colors";
+  };
+
   programs.plasma = {
     enable = true;
     workspace = {
       lookAndFeel = "org.kde.breezedark.desktop";
       wallpaper = ./desktop-wallpaper.jpg;
+      colorScheme = "cyberpunk-neon";
     };
     input.touchpads = [
       {
