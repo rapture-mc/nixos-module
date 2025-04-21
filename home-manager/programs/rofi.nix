27c1da5307d8722f66721d4948eb6e12aa3a1dfd
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-04-21 09:53:26 +0930

    stripping quotes

--- home-manager/programs/rofi.nix
+++ home-manager/programs/rofi.nix
@@ -10,12 +10,11 @@
         primary = mkLiteral "#C5C8C6";
         ug = mkLiteral "#0B2447";
         font = "Monospace 11";
-        background-color = "@bg";
-        # dark = "@bg";
-        border = "0px";
-        kl = "#7aa2f7";
-        black = "#000000";
-        transparent = "rgba(46,52,64,0)";
+        background-color = mkLiteral "@bg";
+        border = mkLiteral "0px";
+        kl = mkLiteral "#7aa2f7";
+        black = mkLiteral "#000000";
+        transparent = mkLiteral "rgba(46,52,64,0)";
       };
 
       "window" = {
