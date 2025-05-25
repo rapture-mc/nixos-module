a79d7ac34103048295550a1e05bbafdf86d76d6d
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-25 15:34:03 +0930

    pinning home-manager to 25.05

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
