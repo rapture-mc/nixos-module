3a736acee81388ce13007125003e7412f399a757
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-27 13:16:41 +0930

    need to add 127.0.0.1/::1 localhost entries

--- modules/services/dnsmasq.nix
+++ modules/services/dnsmasq.nix
@@ -48,7 +48,10 @@ in {
   config = mkIf cfg.enable {
     environment.etc.custom-hosts = {
       enable = true;
-      text = cfg.hosts;
+      text = ''
+        127.0.0.1 localhost
+        ::1 localhost
+      '' + cfg.hosts;
     };
 
     services = {
