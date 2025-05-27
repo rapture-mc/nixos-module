039d12462841868907eec2f230ca865234baa68c
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-27 14:43:01 +0930

    adding more OU's

--- modules/services/openldap/ou-structure.nix
+++ modules/services/openldap/ou-structure.nix
@@ -1,20 +1,29 @@
 { config }: let
   cfg = config.megacorp.services.openldap;
+  ou = "organizationalUnit";
 in ''
   dn: ${cfg.domain-component}
   objectClass: domain
   dc: megacorp
 
   dn: ou=Users,${cfg.domain-component}
-  objectClass: organizationalUnit
+  objectClass: ${ou}
   ou: Users
 
   dn: ou=IT,ou=Users,${cfg.domain-component}
-  objectClass: organizationalUnit
+  objectClass: ${ou}
   ou: IT
 
   dn: ou=Finance,ou=Users,${cfg.domain-component}
-  objectClass: organizationalUnit
+  objectClass: ${ou}
   ou: Finance
+
+  dn: ou=Reception,ou=Users,${cfg.domain-component}
+  objectClass: ${ou}
+  ou: Reception
+
+  dn: ou=Managers,ou=Users,${cfg.domain-component}
+  objectClass: ${ou}
+  ou: Managers
 
 ''
